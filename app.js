516356a2a901124a35bac7e1f436c1412aa722f2
Deepak Sharma <111009426+Deepak9798kumar@users.noreply.github.com>
2024-05-06 12:56:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,9 +73,9 @@ const storage = multer.diskStorage({
   const upload = multer({ storage: storage });
 
   app.post('/upload', upload.single('file'), async (req, res) => {
-  console.log('upload file is running =', req.file);
+  console.log('upload file is running =',req.file)
   try {
-    const filePath = req.file.path.replace(/\\/g, '\'); // Replace backslashes with forward slashes
+    const filePath = req.file.path.replace(/\\/g, '/'); // Replacing backslashes with forward slashes
     const newFile = new File({ url: filePath, type: req.file.mimetype });
     await newFile.save();
     res.status(201).json({ message: 'File uploaded successfully' });
